b0f8a7a3f86b0da07c6a25b0ad3dc5e7b2661a74
janggalman <janggalman@gmail.com>
2020-06-06 15:45:48 +0900

    BaseFragment에서 mConext를 생성 지원 -멤버변수 생성 => onActivityCreated에서 객체화 - lateinit car 를 이용해서 생성

--- app/src/main/java/kr/tjoeun/fragmenttest_20200606/fragments/BaseFragment.kt
+++ app/src/main/java/kr/tjoeun/fragmenttest_20200606/fragments/BaseFragment.kt
@@ -1,9 +1,19 @@
 package kr.tjoeun.fragmenttest_20200606.fragments
 
+import android.content.Context
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment : Fragment() {
 
+    lateinit var mContext: Context
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        프래그먼트를 들고있는 액티비티의 제작이 완료 된 후에 mContext를 대입해주자.
+        mContext = activity as Context
+    }
     abstract fun setupEvents()
     abstract fun setValues()
 
